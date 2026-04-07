93e88d1e27d503094defc79cfc789926ccbdeb96
Emilien-Marot <12394@holbertonstudents.com>
2026-04-07 13:22:12 +0400

    py 0.5.0

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+'''
+Here you go
+'''
+
+
+def sum_list(input_list: list[float]) -> float:
+    '''
+    here we go
+    :param input_list:
+    :return:
+    '''
+    sum: float = 0
+    for val in input_list:
+        sum += val
+    return sum
