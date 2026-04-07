837ec122520f8cbd4569a21b3f6e271e55d7f3eb
Emilien-Marot <12394@holbertonstudents.com>
2026-04-07 11:00:19 +0400

    py 0.1.0

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''
+here you go
+'''
+
+
+def concat(a: str, b: str) -> str:
+    '''
+    here you go
+    :param a:
+    :param b:
+    :return:
+    '''
+    return f"{a}{b}"
