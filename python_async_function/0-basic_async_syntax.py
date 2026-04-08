f12498cbaa0765ab98c5239e33fc057e2840b6ae
Emilien-Marot <12394@holbertonstudents.com>
2026-04-08 09:26:16 +0400

    py 1.0.0

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''
+here you go
+'''
+
+
+async def wait_random(max_delay = 10):
+    '''
+    here we go
+    :param max_delay:
+    :return:
+    '''
+    from asyncio import sleep
+    from random import random
+    delay = max_delay * random()
+    await sleep(delay)
+    return delay
