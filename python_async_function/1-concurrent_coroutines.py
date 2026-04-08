c30084e16d299419f7acb9d879568d4c3acee7fd
Emilien-Marot <12394@holbertonstudents.com>
2026-04-08 10:24:07 +0400

    py 1.1.0.1

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -16,4 +16,9 @@ async def wait_n(n: int, max_delay: int) -> list[float]:
     for i in range(0, n):
         delay = await wait_random(max_delay)
         list_delay.append(delay)
-    return list_delay
+    sorted_list = []
+    for i in range(0, n):
+        min_delay = min(list_delay)
+        list_delay.remove(min_delay)
+        sorted_list.append(min_delay)
+    return sorted_list
