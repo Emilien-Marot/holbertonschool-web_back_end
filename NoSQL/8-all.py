026e0873d2376be6b38c90978556144a37892e81
Emilien-Marot <12394@holbertonstudents.com>
2026-04-23 09:17:54 +0400

    no 0.8.0

---
+++ NoSQL/8-all.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+something something pineapple
+"""
+
+
+def list_all(mongo_collection):
+    """
+    something something walabi
+    :param mongo_collection:
+    :return:
+    """
+    return mongo_collection.find()
