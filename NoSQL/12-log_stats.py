3f96552c78ec00b4fddfbc462184a3e2985ecb27
Emilien-Marot <12394@holbertonstudents.com>
2026-04-23 11:38:46 +0400

    no 0.12.1

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -9,9 +9,11 @@ if __name__ == "__main__":
     db = client.logs
     col = db.nginx
     total_methods = {"GET": 0, "POST": 0, "PUT": 0, "PATCH": 0, "DELETE": 0}
-    total = col.aggregate([{"$match": {}}, {"$count": "total"}])
-    for val in total:
-        print(f"{val['total']} logs")
+    total_col = col.aggregate([{"$match": {}}, {"$count": "total"}])
+    total = 0
+    for val in total_col:
+        total = val['total']
+    print(f"{total} logs")
     methods = col.aggregate([
         {"$group": {"_id": "$method", "count": {"$sum": 1}}}])
     for method in methods:
@@ -20,9 +22,11 @@ if __name__ == "__main__":
     print("Methods:")
     for method in total_methods:
         print(f"    method {method}: {total_methods[method]}")
-    count_status = col.aggregate(
+    col_status = col.aggregate(
         [
             {"$match": {"method": "GET", "path": "/status"}},
             {"$count": "total"}])
-    for status in count_status:
-        print(f"{status['total']} status check")
+    count_status = 0
+    for status in col_status:
+        count_status = status['total']
+    print(f"{count_status} status check")
