a276d4bbcf20fc7fa8eacdc09e2af004b92a0396
Emilien-Marot <12394@holbertonstudents.com>
2026-05-04 10:54:44 +0400

    js 2.1.1

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -2,6 +2,6 @@ export default function getListStudentIds(arrayStudent) {
   if (!Array.isArray(arrayStudent)) {
     return ([]);
   }
-  const mapped = arrayStudent.map((x, idx) => idx);
+  const mapped = arrayStudent.map((x) => x.id);
   return (mapped);
 }
