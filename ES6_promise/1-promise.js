48dbe988a4063d3b822b6fd8bcc07d98a953e2d9
Emilien-Marot <12394@holbertonstudents.com>
2026-05-06 11:01:21 +0400

    js 3.1.4

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -2,7 +2,7 @@ import { uploadPhoto } from './utils';
 
 export default function getFullResponseFromAPI(success) {
   if (success) {
-    return Promise.resolve(uploadPhoto())
+    return Promise.resolve({status: 200, body: 'Success'})
   } else {
     return (Promise.resolve(new Error('The fake API is not working currently')))
   }
