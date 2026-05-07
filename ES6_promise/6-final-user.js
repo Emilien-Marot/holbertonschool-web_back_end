cc92be0d8994ef74047f36fc6ac98d74201799c1
Emilien-Marot <12394@holbertonstudents.com>
2026-05-07 14:18:11 +0400

    js 3.6.5

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,7 +2,7 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([uploadPhoto(fileName), signUpUser(firstName, lastName)])
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((val) => {
       const res = val.map((line) => {
         if (line.reason !== undefined) {
