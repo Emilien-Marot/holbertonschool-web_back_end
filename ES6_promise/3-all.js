a94d802ff76f430b3f06d57b0e40ae4e0ba14bc7
Emilien-Marot <12394@holbertonstudents.com>
2026-05-07 09:18:21 +0400

    js 3.3.1

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,9 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  uploadPhoto().then((res1) => {
-    createUser().then((res2) => {
-      console.log(`${res1.body} ${res2.firstName} ${res2.lastName}`);
-    });
-  }).catch(() => { console.log('Signup system offline'); });
+  Promise.all([uploadPhoto(), createUser()])
+    .then((val) => { console.log(`${val[0].body} ${val[1].firstName} ${val[1].lastName}`); })
+    .catch(() => { console.log('Signup system offline'); });
 }
