716214c89677e57901a76370027321bea75ee13f
Emilien-Marot <12394@holbertonstudents.com>
2026-05-13 09:55:57 +0400

    4.2.0

---
+++ Node_JS_basic/2-read_file.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+
+module.exports = (filename) => {
+  let content;
+  try {
+    content = fs.readFileSync(filename).toString();
+  } catch (e) {
+    throw new Error('Cannot load the database');
+  }
+  const listCsv = content.split('\n');
+  const listCol = listCsv[0].split(',');
+  const listDict = listCsv.filter((a, idx) => a !== '' && idx > 0).map((a) => {
+    const line = a.split(',');
+    const dict = {};
+    for (let i = 0; i < listCol.length; i += 1) {
+      dict[listCol[i]] = line[i];
+    }
+    return (dict);
+  });
+  console.log(`Number of students: ${listDict.length}`);
+  const listField = {};
+  listDict.forEach((line) => {
+    if (listField[line.field] === undefined) {
+      listField[line.field] = [];
+    }
+    listField[line.field].push(line.firstname);
+  });
+  Object.keys(listField).forEach((key) => {
+    const listStudent = listField[key];
+    console.log(`Number of students in ${key}: ${listStudent.length}. List: ${listStudent.join(', ')}`);
+  });
+};
