48b507aed49f7147c45321fe436d54249dc79ed5
Emilien-Marot <12394@holbertonstudents.com>
2026-05-14 09:46:03 +0400

    js 4.7.0

---
+++ Node_JS_basic/7-http_express.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const { promises: fs } = require('fs');
+
+const app = express();
+const port = 1245;
+const [, , filename] = process.argv;
+
+async function readCsv(filename) {
+  let content;
+  let result = 'This is the list of our students\n';
+
+  try {
+    content = (await fs.readFile(filename)).toString();
+  } catch (e) {
+    throw new Error('Cannot load the database');
+  }
+  const listCsv = content.split('\n');
+  const listCol = listCsv[0].split(',');
+  const listDict = listCsv.filter((a, idx) => a !== '' && idx > 0).map((a) => {
+    const line = a.split(',');
+    const dict = {};
+    for (let i = 0; i < listCol.length; i += 1) {
+      dict[listCol[i]] = line[i];
+    }
+    return (dict);
+  });
+  result += `Number of students: ${listDict.length}`;
+  const listField = {};
+  listDict.forEach((line) => {
+    if (listField[line.field] === undefined) {
+      listField[line.field] = [];
+    }
+    listField[line.field].push(line.firstname);
+  });
+  Object.keys(listField).forEach((key) => {
+    const listStudent = listField[key];
+    result += `\nNumber of students in ${key}: ${listStudent.length}. List: ${listStudent.join(', ')}`;
+  });
+  return (result);
+}
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', async (req, res) => {
+  res.send(await readCsv(filename));
+});
+
+app.listen(port);
+
+module.exports = app;
