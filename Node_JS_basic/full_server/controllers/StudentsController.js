c6bb359af0d05be0ad65d12decbfe94a1cc39d39
Emilien-Marot <12394@holbertonstudents.com>
2026-05-16 21:14:41 +0400

    js 4.8.0

---
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -0,0 +1,42 @@
+const readDatabase = require('../utils');
+
+export default class StudentsController {
+  static getAllStudents(request, response) {
+    const argv = process.argv;
+    const path = argv[argv.length - 1];
+    readDatabase(path).catch(() => {
+      response.status(500);
+      response.send('Cannot load the database');
+    }).then((listStudents) => {
+      let result = "EM's code";
+      result = 'This is the list of our students';
+      Object.keys(listStudents).forEach((key) => {
+        const line = listStudents[key];
+        result += `\nNumber of students in ${key}: ${line.length}. List: ${line.join(', ')}`;
+      });
+      response.status(200);
+      response.send(result);
+    });
+  }
+
+  static getAllStudentsByMajor(request, response) {
+    const argv = process.argv;
+    const path = argv[argv.length - 1];
+    const field = request.params.major;
+    if (field !== 'CS' && field !== 'SWE') {
+      response.status(500);
+      response.send('Major parameter must be CS or SWE');
+      return;
+    }
+    readDatabase(path).catch(() => {
+      response.status(500);
+      response.send('Cannot load the database');
+    }).then((listStudents) => {
+      const line = listStudents[field];
+      response.status(200);
+      response.send(`List: ${line.join(', ')}`);
+    });
+  }
+}
+
+module.exports = StudentsController;
---
+++ Node_JS_basic/full_server/utils.js
@@ -0,0 +1,29 @@
+const fs = require('fs').promises;
+
+export default function readDatabase(filename) {
+  return Promise.resolve(fs.readFile(filename))
+    .then((content) => {
+      const listCsv = content.toString().split('\n');
+      const listCol = listCsv[0].split(',');
+      const listDict = listCsv.filter((a, idx) => a !== '' && idx > 0).map((a) => {
+        const line = a.split(',');
+        const dict = {};
+        for (let i = 0; i < listCol.length; i += 1) {
+          dict[listCol[i]] = line[i];
+        }
+        return (dict);
+      });
+      const listField = {};
+      listDict.forEach((line) => {
+        if (listField[line.field] === undefined) {
+          listField[line.field] = [];
+        }
+        listField[line.field].push(line.firstname);
+      });
+      return (listField);
+    }, () => {
+      return (new Error('Cannot load the database'));
+    });
+}
+
+module.exports = readDatabase;
